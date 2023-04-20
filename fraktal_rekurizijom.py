826342dece7c6b65d4f9d74431bfa38eb0f8a691
JosipKis <120599724+JosipKis@users.noreply.github.com>
2023-04-20 14:16:00 +0200

    Add files via upload

---
+++ fraktal_rekurizijom.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+def draw_triangle(x, y, a, ax):
+    plt.plot([x, x+a], [y, y], 'k')
+    v = a ** (3) ** (0.5) / 2
+    plt.plot([x, x+a/2], [y, y+v], 'k')
+    plt.plot([x+a, x+a/2], [y, y+v], 'k')
+
+fig, ax = plt.subplot()
+draw_triangle(0, 0, 100, ak)
