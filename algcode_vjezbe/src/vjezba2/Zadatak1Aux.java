db65596974d5d83d5cda9b373f359eba5968b455
JosipKis <120599724+JosipKis@users.noreply.github.com>
2023-04-22 20:00:37 +0200

    Add files via upload

---
+++ algcode_vjezbe/src/vjezba2/Zadatak1Aux.java
@@ -0,0 +1,64 @@
+package vjezba2;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Zadatak1Aux {
+
+	/* metoda ta generiranje liste; korisnik određuje veličinu, i minimalnu i maksimalnu
+	* granicu za generiranje nasumičnih brojeva
+	*/
+	public static int[] rndLista(int size, int min, int maks) {
+		int[] lista = new int[size];
+		for (int c = 0; c < size; c++) {
+			lista[c] = ThreadLocalRandom.current().nextInt(min, maks);
+		}
+		return lista;
+	}
+	
+	/*
+	 *  metoda za brisanje odabranog elementa
+	 */
+	public static int[] briseClana(int[] lista, int idx) {
+		int[] novaLista = new int[lista.length-1];
+		int br = 0;
+		
+		for (int c = 0; c < lista.length; c++) {
+			if (c == idx) {
+//				br++;
+				continue;
+			}else {
+				novaLista[br] = lista[c];
+				br++;
+				}	
+			}
+		return novaLista;
+	}
+	/*
+	 * metoda koja zamjenjuje određeni element, s posljednjim članom
+	 */
+	public static int[] swapClanova(int[] lista, int clan) {
+		int temp = 0;
+		if (clan == lista.length-1) {
+			return lista;
+		}else {
+			temp = lista[lista.length-1];
+			lista[lista.length-1] = lista[clan];
+			lista[clan] = temp;
+			return lista;
+		}
+	}
+	
+	/*
+	 * 
+	 */
+	public static int[] promjenaClana(int[] lista, int clan, int noviClan) {
+		int temp;
+		if (clan > lista.length-1) {
+			return lista;
+		}else {
+			temp = noviClan;
+			lista[clan] = temp;
+			return lista;
+		}
+	}
+}
---
+++ algcode_vjezbe/src/vjezba2/Zadatak1Main.java
@@ -0,0 +1,28 @@
+package vjezba2;
+
+import java.util.Arrays;
+
+public class Zadatak1Main {
+	/*
+	 * Sadrži zadatke 1, 2, 3.
+	 */
+
+	public static void main(String[] args) {
+
+		int[] Lista = Zadatak1Aux.rndLista(5, 1, 25);
+		System.out.println(Arrays.toString(Lista));
+		int[] novaLis = Zadatak1Aux.briseClana(Lista, 2);
+		System.out.println("Lista nakon brisanja s zadanog indeksa: ");
+		System.out.println(Arrays.toString(novaLis));
+		int[] swapLista = Zadatak1Aux.swapClanova(novaLis, 1);
+		System.out.println("Lista nakon sto zamjenimo posljendnji i neki određeni član: ");
+		System.out.println(Arrays.toString(swapLista));
+		int[] copyOfLista = Arrays.copyOf(swapLista, swapLista.length-1);
+		System.out.println("Lista nakon korištenja copyOf metode: ");
+		System.out.println(Arrays.toString(copyOfLista));
+		int[] listaSnovimClanom = Zadatak1Aux.promjenaClana(copyOfLista, 0, 1000);
+		System.out.println("Lista nakon sto smo ubacili zeljeni clan na zeljeno mjesto: ");
+		System.out.println(Arrays.toString(listaSnovimClanom));
+	}
+
+}
