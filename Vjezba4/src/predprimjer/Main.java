fd9bff92b21446899c745bb7f4d5e6b436bddfc9
JosipKis <josip.kis2@gmail.com>
2023-05-07 19:19:12 +0200

    Vjezba 4 pretprimjer, changes made 07/05/'23, 19:19, gotov zadatak

---
+++ Vjezba4/src/predprimjer/Main.java
@@ -0,0 +1,9 @@
+package predprimjer;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle autic =  new Vehicle();
+        Vehicle auto = new Vehicle("Sportski");
+
+    }
+}
