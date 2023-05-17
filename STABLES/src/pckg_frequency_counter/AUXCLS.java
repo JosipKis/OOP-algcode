56a5b0073a910815244264ae771f6d1483079941
JosipKis <josip.kis2@gmail.com>
2023-05-17 19:16:40 +0200

    predavanje iz alg code 17/5/'23, stables

--- STABLES/src/pckg_frequency_counter/AUXCLS.java
+++ STABLES/src/pckg_frequency_counter/AUXCLS.java
@@ -1,15 +1,67 @@
 package pckg_frequency_counter;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class AUXCLS {
 
+    public static HashMap<Integer, Integer> wordLengthCounter(ArrayList<String> text, int[] length){
+        HashMap<Integer, Integer> freqCounter = new HashMap<>();
+        for (int num: length){
+            freqCounter.put(num, 0);
+        }
+        for (String line: text){
+            String[] words = line.split(" ");
+            System.out.println(Arrays.toString(words));
+            for (String word: words){
+                for (int num: length){
+                    if (num == word.length()){
+                        int value = freqCounter.get(num) + 1;
+                        freqCounter.put(num, value);
+                        break;
+                    }
+                }
+            }
+        }
+
+        return freqCounter;
+    }
+
     public static boolean fileExists(String filePath){
         File file = new File(filePath);
-        boolean exists = file.exists();
+        boolean exists = file.exists() && file.isFile();
         System.out.println("File exists: "+ exists);
         System.out.println("File path: "+ filePath);
 
-        return false; // change to final version
+        return exists;
+    }
+
+    public static ArrayList<String> readDataFromFile(String filePath){
+        ArrayList<String> text = new ArrayList<>();
+        if (fileExists(filePath)){
+            try (BufferedReader br = new BufferedReader(new FileReader(new File(filePath)))){
+                String line;
+                while((line = br.readLine()) != null){
+                    text.add(line);
+                }
+                System.out.println("Reading data from file is finished!");
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }else {
+            System.out.println("Cannot read from file... File doesn't exist.");
+        }
+
+        return text;
+    }
+
+    public static void readTextFromArrayList(ArrayList<String> text, int numLines){
+        for (int k = 0; k < numLines; k++){
+            System.out.println(text.get(k));
+        }
     }
 }
--- STABLES/src/pckg_frequency_counter/Test.java
+++ STABLES/src/pckg_frequency_counter/Test.java
@@ -1,9 +1,18 @@
 package pckg_frequency_counter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Test {
 
     public static void main(String[] args) {
-        String filePath = "STABLES/src/pckg_frequency_counter/quotes.txt";
-        AUXCLS.fileExists(filePath);
+        String filePath = "STABLES\\src\\pckg_frequency_counter\\quotes.txt";
+        ArrayList<String> text = AUXCLS.readDataFromFile(filePath);
+        AUXCLS.readTextFromArrayList(text, 10);
+        int[] lngth = {3, 4, 7, 11, 15};
+        HashMap<Integer, Integer> freqCounter = AUXCLS.wordLengthCounter(text, lngth);
+        for (Integer key: freqCounter.keySet()){
+            System.out.println("N= "+ key +" | freq: "+ freqCounter.get(key));
+        }
     }
 }
