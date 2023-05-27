75abe4c2a1261e792e9c03675e93f0b6a0d884d1
JosipKis <josip.kis2@gmail.com>
2023-05-27 16:04:14 +0200

    vjezba 5b, klasa Customer i Main klasa 27/05/'23

---
+++ vjezba5b/src/Customer.java
@@ -0,0 +1,29 @@
+public class Customer {
+
+    private String adress;
+    private int id;
+    private String name;
+
+    public Customer(String name, String adress){
+        this.name = name;
+        this.adress = adress;
+        id = 20;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "adress='" + adress + '\'' +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
