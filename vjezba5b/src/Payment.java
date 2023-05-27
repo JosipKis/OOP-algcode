2b3bbc688ad44e5adfe204f2d859131c3f53b1f9
JosipKis <josip.kis2@gmail.com>
2023-05-27 16:46:26 +0200

    vjezba 5b, Payment klasa 27/05/'23

---
+++ vjezba5b/src/Payment.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Payment {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    Payment(){
+
+    }
+
+    private void possiblePaymentMethods(){
+        System.out.println("Possible Payment options are: ");
+        System.out.println("COD - Cash On Delivery");
+        System.out.println("CC/DC - Credit Card / Debit Card");
+        System.out.println("PP - PayPal Service");
+    }
+
+    void payPackage(){
+        boolean paymentStatus = true;
+        possiblePaymentMethods();
+        System.out.println("Please enter your payment option: ");
+        while (paymentStatus){
+            String choosePayment = scanner.nextLine();
+            if ("COD".equals(choosePayment)){
+                System.out.println("Payment method - Cash on delivery");
+                paymentStatus = false;
+            } else if ("CC/DC".equals(choosePayment)) {
+                System.out.println("Payment method - credit / debit card");
+                paymentStatus = false;
+            } else if ("PP".equals(choosePayment)) {
+                System.out.println("Payment method - PayPal service");
+                paymentStatus = false;
+            }else {
+                System.out.println("Unsupported payment option - try again...");
+            }
+        }
+    }
+}
--- vjezba5b/src/TestWebShop.java
+++ vjezba5b/src/TestWebShop.java
@@ -3,5 +3,7 @@ public class TestWebShop {
     public static void main(String[] args) {
         Customer customer1 = new Customer("Josip", "Juan Tabo 6969");
         System.out.println(customer1.toString());
+        Payment payment = new Payment();
+        payment.payPackage();
     }
 }
