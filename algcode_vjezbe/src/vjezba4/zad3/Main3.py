543794a8613607f4c774531735c52add300b776b
JosipKis <josip.kis2@gmail.com>
2023-06-09 13:53:40 +0200

    Alg code vjezba 4, Rekurzija, zad3 Python solution commit

--- algcode_vjezbe/src/vjezba4/zad3/Main3.py
+++ algcode_vjezbe/src/vjezba4/zad3/Main3.py
@@ -1,35 +1,42 @@
 from timeit import default_timer as timer
+from typing import List
 
-
-''' 
-    U Pytohnu za 100 i 10000 vrijeme programa sa rekurzijom je bilo gotovo 2 puta sporije
-    dok je za 1000000 više od nekoliko puta sporiji od petlje...
+'''
+    Rezultati u Pythonu su takvi da je rekurzijska funkcija sporija od normalne funkcije za testove 100 i 10000
+    sporija za otprilike 10, puta, a za test 1000000 je sporija za cak 3 sekunde.
 '''
 
-
-def recursion_potentions(x: int, n: int):
-    if n == 0:
-        return 1
-    elif n % 2 == 0:
-        return (x ** (n - 1)) ** 2
+def recursion_potentions(x: int, n: List[int]):
+    if x == 0:
+        return "X equals 0"
+    elif len(n) == 0:
+        return "Done"
     else:
-        return x * (x ** (n - 1)) ** 2
+        potention = x ** n[0]
+        return recursion_potentions(x, n[1:])
 
 
-def normal_potentions(x: int, n: int):
-    for i in range(n):
+def normal_potentions(x: int, n: List[int]):
+    for i in n:
         potention = x ** i
         return potention
 
 
-if __name__ == '__main__':
+def timerr(repeats):
+    inti = [2, 5, 7, 11, 15, 25, 30, 50, 63]
     star_timer = timer()
-    recursion_potentions(2, 1000000)
+    for i in range(repeats):
+        recursion_potentions(2, inti)
     end_timer = timer()
     print("Recursion time: ", end_timer - star_timer)
     print("--------------------------------------------------")
     star_timer = timer()
-    normal_potentions(2, 1000000)
+    for i in range(repeats):
+        normal_potentions(2, inti)
     end_timer = timer()
     print("Normal time: ", end_timer - star_timer)
+
+
+if __name__ == '__main__':
+    timerr(1000000)
 
