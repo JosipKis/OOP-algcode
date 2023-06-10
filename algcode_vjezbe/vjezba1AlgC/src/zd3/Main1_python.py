1911f6402ed9a93705715513c698598b0c1653c5
JosipKis <josip.kis2@gmail.com>
2023-06-10 20:08:54 +0200

    alg code vjezba1 zd3, Java solution commit

---
+++ algcode_vjezbe/vjezba1AlgC/src/zd3/Main1_python.py
@@ -0,0 +1,27 @@
+import random
+from typing import List
+
+MAX = 300
+MIN = 0
+
+
+def generate_rnd_arr(size: int):
+    rnd_arr = []
+    for i in range(size):
+        rnd_arr.append(random.randint(MIN, MAX))
+    return rnd_arr
+
+
+def find_max_in_arr(arr: List):
+    max = 0
+    for i in arr:
+        if i > max:
+            max = i
+    print("Maximum number in the list is: ", max)
+
+
+if __name__ == '__main__':
+    print("This list is randomly generated:")
+    lst = generate_rnd_arr(10)
+    print(lst)
+    find_max_in_arr(lst)
